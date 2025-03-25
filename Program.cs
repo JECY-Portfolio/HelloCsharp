f36763e156a0b102ad2ecff05302985d1228f716
Geofrrey Jecinta <120319299+JECY-Portfolio@users.noreply.github.com>
2025-03-25 18:18:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+    {
+        Console.WriteLine("THIS PROGRAM CALCULATES THE AVERAGE GRADE OF STUDENTS USING C#");
+
+
+        Console.Write("Enter Student's Name: ");
+        string studentName = Console.ReadLine();
+
+
+        Console.Write("Enter Number of Grades: ");
+        int numberOfGrades;
+
+      
+        while (!int.TryParse(Console.ReadLine(), out numberOfGrades) || numberOfGrades <= 0)
+        {
+            Console.Write("Invalid number, please enter a POSITIVE number: ");
+        }
+
+        double sum = 0;
+
+        
+        for (int i = 1; i <= numberOfGrades; i++) 
+        {
+            double grade;
+            Console.Write("Please enter grade number " + i + ": ");
+
+            
+            while (!double.TryParse(Console.ReadLine(), out grade) || grade <= 0)
+            {
+                Console.Write("Invalid grade: please enter a POSITIVE number: ");
+            }
+
+            sum = sum + grade; 
+        }
+
+    Console.WriteLine(studentName + "'s average grade is " + (sum / numberOfGrades).ToString("F2"));
+    }
+
